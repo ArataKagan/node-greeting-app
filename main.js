1316fa0755864efca10d2b2162bf95a26b6af5b8
Arata Kagan <aratakagan@gmail.com>
2018-10-25 14:12:05 -0700

    greeting app files added

---
+++ main.js
@@ -0,0 +1,17 @@
+const greetings = require('./greeting.js'); 
+const weatherPhrase = require('./weather.js');
+
+// 'Hello'
+greetings.greetingEnglish();
+
+//'Konnichiwa' 
+greetings.greetingJapanese(); 
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+weatherPhrase.temperture();
+
+weatherPhrase.weather();
+
---
+++ weather.js
@@ -0,0 +1,7 @@
+exports.temperture =  function() {
+    console.log("Today's temperature is 60 degree");
+}
+
+exports.weather = function(){
+    console.log("Today's weather is sunny!");
+}
